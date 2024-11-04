75cd9bd0dd942e3001e04afa1f68a6c5dfd3d987
orlando <orlando@orlandodeMacBook-Air.local>
2024-11-04 11:23:49 +0800

    Initial commit

---
+++ try.py
@@ -0,0 +1,11 @@
+# Variables and Data Types
+temperature = 25 # integer
+humidity = 70.5 # float
+city = "New York" # string
+is_raining = False # boolean a
+
+# Printing variables and their types
+print("Temperature:", temperature, "Type:", type(temperature))
+print("Humidity:", humidity, "Type:", type(humidity))
+print("City:", city, "Type:", type(city))
+print("Is it raining?", is_raining, "Type:", type(is_raining))
